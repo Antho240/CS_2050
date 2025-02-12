a0b85e68070e45f5fee9be0cae7090a116bef48a
Anthony Stuart <anthonynstuart@gmail.com>
2025-02-12 15:18:36 -0600

    update prelab_main.c for prelab3

--- labs/lab3/prelab_main.c
+++ labs/lab3/prelab_main.c
@@ -7,11 +7,6 @@ int main(void){
     fleet = load_fleet(filename);
     char fastest_class = class_with_highest_average_speed(fleet);
     printf("Class with greatest average speed is %c!\n",fastest_class);
-
-
     free_fleet(fleet);
-   
-
-
     return 0;
 }
