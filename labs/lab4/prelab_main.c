707e58d5e5fae722b948d57fea23b172d9f66d2d
Anthony Stuart <anthonynstuart@gmail.com>
2025-02-20 14:40:34 -0600

    add prelab_main.c for prelab 4

--- labs/lab4/prelab_main.c
+++ labs/lab4/prelab_main.c
@@ -2,36 +2,54 @@
 
 int main(void){
     FILE* fp = NULL;
-    Employee* employees = NULL;
-    employees = readEmployeeArray(fp);
+    Employee* emps = NULL;
+    emps = readEmployeeArray(fp);
+    Employee* emp1 = NULL;
+    int id = 11000;
+    emp1 = getEmployeeByID(emps, id);
+    if(emp1 != NULL){
+        printf("Employee with ID #%d info: %d, %d, %.2f\n", id, emp1->empID, emp1->jobType, emp1->salary);
+    }
     
-    if(setEmpSalary(employees, 14000, 154000.00)){
-        printf("***Set Employee salary failed***\n");
-    }
-    float salary;
-    if(getEmpSalary(employees, 14000, &salary)){
-        printf("***Get Employee salary failed***\n");
+    float salary=0;
+    if(getEmpSalary(emps, id, &salary)){
+        printf("***Failed to get salary***\n");
     }
     else{
-        printf("Salary of employee from lookup: %.2f\n", salary);
+        printf("Employee with ID #%d salary: %.2f\n", id, salary);
     }
 
-    if(setEmpJobType(employees, 14000, 5)){
-        printf("***Set job type failed***\n");
-    }
-    int job;
-    if(getEmpJobType(employees, 14000, &job)){
-        printf("***Get job type failed***\n");
+    int jobtype;
+    if(getEmpJobType(emps, id, &jobtype)){
+        printf("***Failed to get jobtype***\n");
     }
     else{
-        printf("Job type of employee from lookup: %d\n", job);
+        printf("Employee with ID #%d jobtype: %d\n", id, jobtype);
     }
-   
+
+    int set_salary = 70000;
+    if(setEmpSalary(emps, id, set_salary)){
+        printf("***Failed to set salary***\n");
+    }
+    else{
+        getEmpSalary(emps, id, &salary);
+        printf("Employee with ID #%d salary updated to: %.2f\n", id, salary);
+    }
+
+    int set_jobtype = 2;
+    if(setEmpJobType(emps, id, set_jobtype)){
+        printf("***Failed to set jobtype***\n");
+    }
+    else{
+        getEmpJobType(emps, id, &jobtype);
+        printf("Employee with ID #%d jobtype updated to: %d\n", id, jobtype);
+    }
 
 
-    if(!freeArray(&employees)){
-        printf("memory succesfully freed\n");
+    
+    freeArray(&emps);
+    if(emps==NULL){
+        printf("***memory freed succesfully***\n");
     }
-    //printf("address after free: %p\n", employees);
     return 0;
 }
